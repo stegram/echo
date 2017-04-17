045ccd11ca68f040340b687cb3d7ec267805f72a
Stefan Gramfält <stefangramfalt@gmail.com>
2017-04-17 22:57:20 +0200

    Use full user object from Firebase

--- js/echoService.js
+++ js/echoService.js
@@ -1,33 +1,17 @@
 echoApp.factory('echo',function ($resource, $cookieStore, $firebaseArray, $firebaseObject) {
-
-
-	var echoUsers = {
-		'cristina' : {'title' : "bma", 'password' : "hejhej", 'lastlogin' : "2017-03-29",
-			'data' : {}},
-		'admin' : {'title' : "admin", 'password' : "null", 'lastlogin' : "2017-03-29",
-			'data' : {}}
-	};
-
-	this.getRegisteredUsers = function(){
-		return echoUsers;
-	};
+	var currentUser = {};
 
 	this.checkUser = function(username){
 		var ref = firebase.database().ref().child('users').child(username);
 		return $firebaseObject(ref);
 	}
 
-	this.setUser = function(name){
-		user = name;
+	this.setUser = function(user){
+		currentUser = user;
 	};
 
 	this.getUser = function(){
-
-		var loggedUser = echoUsers[user];
-
-		loggedUser.name = user;
-
-		return loggedUser;
+		return currentUser;
 	};
 
 	this.judge = function (exam) {
@@ -61,7 +45,7 @@ echoApp.factory('echo',function ($resource, $cookieStore, $firebaseArray, $fireb
 
 	this.getTakenExams = function () {
 		// Get all the users taken exams
-		var ref = firebase.database().ref().child("users").child(this.getUser().name).child("exams");
+		var ref = firebase.database().ref().child("users").child(this.getUser().$id).child("exams");
 		return $firebaseArray(ref);
 	}
 
