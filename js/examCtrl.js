37a0441f03ae741938f15b233f46e97e75cf41db
Stefan Gramfält <stefangramfalt@gmail.com>
2017-04-18 13:11:47 +0200

    Mde possible to view old exams
    Added an extra variable to toggle and added a funtion for getting both
    old and new exams depending on the state of the toggle variable

--- js/examCtrl.js
+++ js/examCtrl.js
@@ -1,10 +1,17 @@
 echoApp.controller('examCtrl', function ($scope, echo) {
 
-	$scope.show = true;
+	$scope.showExamList = true;
+	$scope.showOldExams = false;
 	$scope.exam;
 	$scope.i = -1;
 
-	$scope.formData = {};
+	$scope.toggleExamList = function(){
+		$scope.showExamList = !$scope.showExamList;
+	}
+
+	$scope.toggleOldExams = function(){
+		$scope.showOldExams = !$scope.showOldExams;
+	}
 
 	$scope.setExam = function(exam){
 		$scope.exam = exam;
@@ -12,28 +19,28 @@ echoApp.controller('examCtrl', function ($scope, echo) {
 
 	$scope.setI = function(i){
 		$scope.i = i;
-		console.log(i);
 	};
 
 	$scope.getI = function(){
 		return $scope.i;
 	};
 
-
-	$scope.toggle = function(){
-		$scope.show = !$scope.show;
-	}
-
-	$scope.clearExam = function() {
-		$scope.exams = echo.getAllExams();
-	}
-
 	$scope.exams = echo.getAllExams();
 
+	$scope.getExams = function(){
+		if($scope.showOldExams) {
+			$scope.exams = echo.getTakenExams();
+		}else if (!$scope.showOldExams){
+		$scope.exams = echo.getAllExams();
+		} else {
+			//console.console.log("badshit");
+		}
+
+	};
 
 
 	<!-- SKRÄP -->
-	
+
 	$scope.submit = function(){
 		var result = echo.judge($scope.exam);
 		echo.saveTakenExam($scope.exam);
