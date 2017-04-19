5672b5b235e1e0847dd0a2ccae9079554ce51c12
robinjac <robinjac@kth.se>
2017-04-19 21:20:50 +0200

    made some polish
    fixed login html. Added logout effect. Added spinners to http requests

--- js/dashboardCtrl.js
+++ js/dashboardCtrl.js
@@ -1,10 +1,29 @@
-echoApp.controller('dashboardCtrl', function ($scope, echo) {
+echoApp.controller('dashboardCtrl', function ($scope, echo,$location) {
 
 $scope.user = echo.getLoginUser();
 
+
 $scope.logout = function(){
-	if($scope.user.name != undefined)
-		echo.lastLogin.update({name:$scope.user.name}, {lastlogin:echo.getCurrentDate()});
+	
+	$('#logout').modal({
+		backdrop: 'static',
+		keyboard: false
+	})
+	
+	$scope.onExit = true;
+	
+	if($scope.user.name != undefined){
+		echo.lastLogin.update({name:$scope.user.name}, {lastlogin:echo.getCurrentDate()},function(){
+			
+			$('#logout').modal('hide');
+			$('body').removeClass('modal-open');
+			$('.modal-backdrop').remove();
+			
+			$location.path('/login');
+			$scope.onExit = false;
+		});
+	};
+		
 };
 
 });
