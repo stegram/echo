6dc99a28e2277b1099cff45397ce67d88b960c0b
Stefan Gramfält <stefangramfalt@gmail.com>
2017-04-20 00:09:18 +0200

    All userinfo to one database
    Trying to isolate userinfo and log data to different databases.
    Userobject is buggy, last login and username in dashboard dissapears
    when pressing submit exam and when submitting the first time login form.

--- js/dashboardCtrl.js
+++ js/dashboardCtrl.js
@@ -1,29 +1,43 @@
-echoApp.controller('dashboardCtrl', function ($scope, echo,$location) {
+echoApp.controller('dashboardCtrl', function ($scope, echo,$location,$timeout) {
 
 $scope.user = echo.getLoginUser();
 
 
 $scope.logout = function(){
-	
+
 	$('#logout').modal({
 		backdrop: 'static',
 		keyboard: false
 	})
-	
-	$scope.onExit = true;
-	
-	if($scope.user.name != undefined){
-		echo.lastLogin.update({name:$scope.user.name}, {lastlogin:echo.getCurrentDate()},function(){
-			
+
+	//$scope.onExit = true;
+
+	//if($scope.user.name != undefined){
+
+		echo.setLastLogin();
+		//bort
+
+		$timeout(function(){
+
 			$('#logout').modal('hide');
 			$('body').removeClass('modal-open');
 			$('.modal-backdrop').remove();
-			
+
 			$location.path('/login');
-			$scope.onExit = false;
-		});
-	};
-		
+		},1000);
+
+		/*
+		echo.lastLogin.update({name:$scope.user.name}, {lastlogin:echo.getCurrentDate()},function(){
+
+		$('#logout').modal('hide');
+		$('body').removeClass('modal-open');
+		$('.modal-backdrop').remove();
+
+		$location.path('/login');
+		$scope.onExit = false;
+	});*/
+	//};
+
 };
 
 });
